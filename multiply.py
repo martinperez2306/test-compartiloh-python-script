c2d15a49af979884cd1b160032428516b668e61f
FerFabbiano <ffabbiano@fi.uba.ar>
2023-11-06 23:48:07 -0300

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -1,20 +1,13 @@
 # multiply.py
 
 import datetime
-import os
-
-# Function to get the absolute path of the script's directory
-def get_script_directory():
-    return os.path.dirname(os.path.abspath(__file__))
 
 # Function to log a message to the output.log file
 def log_message(message):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     log_entry = f"[{timestamp}] {message}\n"
     
-    log_file_path = os.path.join(get_script_directory(), "output.log")
-    
-    with open(log_file_path, "w") as log_file:
+    with open("/app/output.log", "a") as log_file:
         log_file.write(log_entry)
 
 # Example usage
@@ -28,3 +21,4 @@ log_message(f"Result: {result}")
 
 # Additional log messages
 log_message("Script finished.")
+
