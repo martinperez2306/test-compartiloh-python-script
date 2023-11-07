c74776598b4c21092de6f047e51524902bfa32f7
FerFabbiano <ffabbiano@fi.uba.ar>
2023-11-06 23:42:59 -0300

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -5,7 +5,7 @@ def log_message(message):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     log_entry = f"[{timestamp}] {message}\n"
     
-    with open("/app/output.log", "a") as log_file:
+    with open("output.log", "a") as log_file:
         log_file.write(log_entry)
 
 # Example usage
