758b9c2af946437ac007c25c87a6d4c1e6f40bdc
FerFabbiano <ffabbiano@fi.uba.ar>
2023-11-08 15:56:58 -0300

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -1,5 +1,12 @@
+import os
 import logging
 
-logging.basicConfig(filename='output.log', level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
+# Get the absolute path to the current directory
+current_dir = os.path.abspath(os.path.dirname(__file__))
+
+# Specify the log file name and create the absolute path
+log_file_path = os.path.join(current_dir, 'output.log')
+
+logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
 
 logging.info("Hello World!")
