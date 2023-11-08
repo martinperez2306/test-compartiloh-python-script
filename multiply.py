d89a32050fb674163f4af5dd87578eb525083201
FerFabbiano <ffabbiano@fi.uba.ar>
2023-11-08 16:00:24 -0300

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -5,7 +5,7 @@ import logging
 current_dir = os.path.abspath(os.path.dirname(__file__))
 
 # Specify the log file name and create the absolute path
-log_file_path = os.path.join(current_dir, 'output.log')
+log_file_path = os.path.join(current_dir, 'app.log')
 
 logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
 
